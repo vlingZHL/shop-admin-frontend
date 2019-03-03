f7865cb65e68b016d114e2bbea512c0409e1e643
vling <vling@163.com>
2019-03-03 09:17:01 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>登录</p>
+    <el-form label-width="100px" class="demo-ruleForm">
+
+      <el-form-item label="账号" class="coutant">
+        <el-input v-model="input" placeholder="请输入登录账号"></el-input>
+      </el-form-item>
+
+      <el-form-item label="密码" >
+        <el-input type="password" autocomplete="off" v-model="pas"></el-input>
+      </el-form-item>
+  
+
+      <el-form-item class="hl-cen">
+        <el-button type="primary"  @click="hlsubmit">提交</el-button>
+        <el-button @click="hlreset">重置</el-button>
+      </el-form-item>
+
+    </el-form>
+
   </div>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    data: function(){
+      return {
+        input: "",
+        pas: ""
+      }
+    },
+    methods: {
+      hlsubmit: function(){
+        console.log("账号："+ (+this.input),"密码："+(+this.pas));
+      },
+      hlreset: function(){
+        this.input = "" ,
+        this.pas = ""
+      }
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+<style lang="scss">
+  #app {
+    margin: 0 auto;
+    width: 500px;
+    height: 300px;
+    p {
+      margin: 0 auto;
+      text-align: center;
+      margin-bottom: 20px;
+      font-size: 20px;
+      color: #409EFF;
+    }
+    
+    .hl-cen {
+      text-align: center;
+    }
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入element-ui
+import ElementUI from 'element-ui';
+// 引入element-ui样式
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 引入axios
+import axios from 'axios';
+
+// 全局注册组件
+Vue.use(ElementUI);
 Vue.config.productionTip = false
+
+// 把axios绑定到vue实例的属性$axios
+Vue.prototype.$axios = axios;
 
 new Vue({
   render: h => h(App),
